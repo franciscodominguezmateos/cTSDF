22011686e820f3c25a629bb10ff2db462c26fca0
Francisco Dominguez <francisco.dominguez@urjc.es>
2015-11-06 00:53:51 +0100

    Added buildMesh procedure

--- cTSDF/src/cTSDF.cpp
+++ cTSDF/src/cTSDF.cpp
@@ -19,7 +19,6 @@ extern "C" {
 using namespace std;
 
 GLint ancho=400;
-
 GLint alto=400;
 int hazPerspectiva = 1;
 GLfloat angle = 0.0;
@@ -28,7 +27,7 @@ GLfloat roll = 0.0;
 GLfloat pitch = 0.0;
 
 DepthImage di1,di2;
-Tsdf<float> t(8);
+Tsdf<float> t(64);
 vector<Point3f> vpts;
 vector<TRIANGLE> mesh;
 
@@ -59,19 +58,19 @@ void displayMe(void)
           glColor4f(1,0,0.0,0.1);
           glNormal3f(t.n[0].x,-t.n[0].y,-t.n[0].z);
     	  glVertex3f(t.p[0].x,-t.p[0].y,-t.p[0].z);
-          glColor4f(0,1,0,0.1);
+          //glColor4f(0,1,0,0.1);
           glNormal3f(t.n[1].x,-t.n[1].y,-t.n[1].z);
     	  glVertex3f(t.p[1].x,-t.p[1].y,-t.p[1].z);
-          glColor4f(0,0,1,0.1);
+          //glColor4f(0,0,1,0.1);
           glNormal3f(t.n[2].x,-t.n[2].y,-t.n[2].z);
     	  glVertex3f(t.p[2].x,-t.p[2].y,-t.p[2].z);
       }
       glEnd();
       glColor3f(1,1,0);
-	  glBegin(GL_POINTS);
-      for(Point3f p:vpts)
-    	  glVertex3f(p.x,-p.y,-p.z);
-      glEnd();
+//	  glBegin(GL_POINTS);
+//      for(Point3f p:vpts)
+//    	  glVertex3f(p.x,-p.y,-p.z);
+//      glEnd();
     glPopMatrix();
     glutSwapBuffers();
     angle++;
@@ -79,7 +78,7 @@ void displayMe(void)
 
 void init (void) {
     glEnable (GL_DEPTH_TEST);
-    glDisable(GL_CULL_FACE);
+    //glDisable(GL_CULL_FACE);
     glEnable (GL_LIGHTING);
     glEnable(GL_COLOR_MATERIAL);
     glEnable (GL_LIGHT0);
@@ -185,57 +184,7 @@ void computeNormals(TRIANGLE *t){
 	t->n[2].y=n.y;
 	t->n[2].z=n.z;
 }
-#define sqr(x) ((x)*(x))
-int main(int argc, char** argv)
-{
-	string basepath;
-	if ( argc != 2 )
-		{
-	        printf("usage: cRGB pathToRGBDdataset \n");
-	        return -1;
-	}
-	basepath=argv[1];
-	int posI=70;
-	DepthImage dImg1(basepath,posI);
-	DepthImage dImg2(basepath,posI+1);
-    di1=dImg1;//.sparse();
-    di2=dImg2.sparse();
-    //di=dImg1;
-    cout << di1.getCentroid()<< " centroid"<<endl;
-    cout << di1.getPoints3D().size()/1000 << "mil filtered points" <<endl;
-    t.clear(0.0);
-    t.setMinMax(-0.35,0.35);
-    vector<Point3f> pts=di1.getPoints3D();
-    cout << "pts.size()" << pts.size() <<endl;
-//    for(Point3f p:pts){
-//    	t.setVoxel(p.x,p.y,p.z,1.0);
-//    }
-    //build sphere
-    for(int i=0;i<t.getSize();i++)
-    	for(int j=0;j<t.getSize();j++)
-    		for(int k=0;k<t.getSize();k++){
-    			float x=t.i2f(i);
-    			float y=t.i2f(j);
-    			float z=t.i2f(k);
-    			float p=0.15;
-    			float d0=sqrt(sqr(x)+sqr(y)+sqr(z))-0.25;
-    			float d1=sqrt(sqr(x-p)+sqr(y-p)+sqr(z-p))-0.125;
-    			float d=fmin(d0,d1);
-    			t.setVoxel(i,j,k,d);
-    		}
-    //get points in voxel
-    for(int i=0;i<t.getSize();i++)
-    	for(int j=0;j<t.getSize();j++)
-    		for(int k=0;k<t.getSize();k++){
-    			//if(t.getVoxel(i,j,k)<=0.1){
-    				float x=t.i2f(i);
-    				float y=t.i2f(j);
-    				float z=t.i2f(k);
-    				Point3f p(x,y,z);
-    				vpts.push_back(p);
-    			//}
-    		}
-    cout << vpts.size() <<endl;
+void buildMesh(Tsdf<float> &t,vector<TRIANGLE> &mesh){
 	TRIANGLE triangles[10];
     GRIDCELL grid;
     for(int i=0;i<t.getSize()-1;i++){
@@ -281,6 +230,61 @@ int main(int argc, char** argv)
     			}
     		}
     }
+}
+#define sqr(x) ((x)*(x))
+int main(int argc, char** argv)
+{
+	string basepath;
+	if ( argc != 2 )
+		{
+	        printf("usage: cRGB pathToRGBDdataset \n");
+	        return -1;
+	}
+	basepath=argv[1];
+	int posI=0;
+	DepthImage dImg1(basepath,posI);
+	DepthImage dImg2(basepath,posI+1);
+    di1=dImg1;//.sparse();
+    di2=dImg2.sparse();
+    //di=dImg1;
+    cout << di1.getCentroid()<< " centroid"<<endl;
+    cout << di1.getPoints3D().size()/1000 << "mil filtered points" <<endl;
+    t.clear(0.0);
+    t.setMinMax(-0.35,0.35);
+    vector<Point3f> pts=di1.getPoints3D();
+    cout << "pts.size()" << pts.size() <<endl;
+//    for(Point3f p:pts){
+//    	t.setVoxel(p.x,p.y,p.z,1.0);
+//    }
+    //build sphere
+    for(int i=0;i<t.getSize();i++)
+    	for(int j=0;j<t.getSize();j++)
+    		for(int k=0;k<t.getSize();k++){
+    			float x=t.i2f(i);
+    			float y=t.i2f(j);
+    			float z=t.i2f(k);
+    			float p=0.20;
+    			float d0=sqrt(sqr(x)+sqr(y)+sqr(z))-0.25;
+    			float d1=sqrt(sqr(x-p)+sqr(y-p)+sqr(z-p))-0.125;
+    			float d2=sqrt(sqr(x+p)+sqr(y-p)+sqr(z-p))-0.125;
+    			float d=fmin(fmin(d0,d1),d2);
+    			//float d=d0+d1;
+    			t.setVoxel(i,j,k,d);
+    		}
+    //get points in voxel
+    for(int i=0;i<t.getSize();i++)
+    	for(int j=0;j<t.getSize();j++)
+    		for(int k=0;k<t.getSize();k++){
+    			//if(t.getVoxel(i,j,k)<=0.1){
+    				float x=t.i2f(i);
+    				float y=t.i2f(j);
+    				float z=t.i2f(k);
+    				Point3f p(x,y,z);
+    				vpts.push_back(p);
+    			//}
+    		}
+    cout << vpts.size() <<endl;
+    buildMesh(t,mesh);
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     //glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
