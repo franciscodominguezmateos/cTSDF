a93b442b95db5a4d65254346178221537fe1fd4b
Francisco Dominguez <francisco.dominguez@urjc.es>
2016-07-08 15:44:12 +0200

    added getLeaves and getNodesAtLevel in GridOctreeNode

--- cTSDF/src/grid_octree_node.h
+++ cTSDF/src/grid_octree_node.h
@@ -7,7 +7,9 @@
 
 #ifndef GRID_OCTREE_NODE_H_
 #define GRID_OCTREE_NODE_H_
+#include <vector>
 
+using namespace std;
 
 template <class T>
 class GridOctreeNode {
@@ -24,6 +26,42 @@ public:
 	}
 	GridOctreeNode **getChildren(){
 		return children;
+	}
+	bool isLeaf(){
+		return children[0]==NULL & children[1]==NULL &
+			   children[2]==NULL & children[3]==NULL &
+			   children[4]==NULL & children[5]==NULL &
+			   children[6]==NULL & children[6]==NULL;
+	}
+	vector<T*> getLeaves(){
+		vector<T*> v;
+		if(isLeaf()){
+			v.push_back(value);
+		}
+		else{
+			for(int i=0;i<8;i++){
+				if(children[i]!=NULL){
+					vector<T*> vc=children[i].getLeaves();
+					v.insert(vc.begin(),vc.end());
+				}
+			}
+		}
+		return v;
+	}
+	vector<GridOctreeNode*> getNodesAtLevel(int level){
+		vector<GridOctreeNode*> v;
+		if(level==0){
+			v.push_back(this);
+		}
+		else{
+			for(int i=0;i<8;i++){
+				if(children[i]!=NULL){
+					vector<GridOctreeNode*> vc=children[i].getNodesAtLevel(level-1);
+					v.insert(vc.begin(),vc.end());
+				}
+			}
+		}
+		return v;
 	}
 };
 
